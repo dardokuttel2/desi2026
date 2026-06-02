19880beb4a121b7b989af988908c79452517681a
dardokuttel <dardokuttel@gmail.com>
2026-06-02 07:47:04 -0300

    se reordenan imports

--- src/main/java/tuti/desi/entidades/Ciudad.java
+++ src/main/java/tuti/desi/entidades/Ciudad.java
@@ -1,10 +1,10 @@
 package tuti.desi.entidades;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Id;
+
 @Entity
 public class Ciudad {
 	
